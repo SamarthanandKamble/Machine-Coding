439a08d8352d6a625341309a155e36b8f3d7b648
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-11-11 12:18:43 +0530

    Multiple accordion open implemented

--- Accordion/script.js
+++ Accordion/script.js
@@ -1,9 +1,10 @@
 const multipleOpenToggler = document.querySelector("#multiple-open-toggler");
+const accordionMainDiv = document.querySelector("#accordion-main-div");
+const accordionText = document.querySelectorAll(".accordion-text");
+let isAccordionOpen = false;
 let multipleOpenAllowed;
 let presentAccordion;
-let isAccordionOpen = false;
-const accordionMainDiv = document.querySelector("#accordion-main-div");
-let previousAccordion = "";
+let item;
 
 function checkMultipleOpenAllowed() {
   if (multipleOpenToggler.checked) {
@@ -15,67 +16,103 @@ function checkMultipleOpenAllowed() {
   }
 }
 
-function toggleAccordionDiv(e) {
-  presentAccordion = e.target;
-
-  if (
-    presentAccordion?.lastElementChild?.classList?.contains("accordion-text") ||
-    presentAccordion?.lastElementChild?.classList?.contains(
-      "accordion-text-active"
-    )
-  ) {
-    isAccordionOpen
-      ? closeAccordion(presentAccordion?.lastElementChild)
-      : openAccordion(presentAccordion?.lastElementChild);
-  } else if (
-    presentAccordion?.nextElementSibling?.nextElementSibling?.classList?.contains(
-      "accordion-text"
-    ) ||
-    presentAccordion?.nextElementSibling?.nextElementSibling?.classList?.contains(
-      "accordion-text-active"
-    )
-  ) {
-    isAccordionOpen
-      ? openAccordion(presentAccordion?.nextElementSibling?.nextElementSibling)
-      : closeAccordion(
-          presentAccordion?.nextElementSibling?.nextElementSibling
-        );
-  } else if (
-    presentAccordion?.nextElementSibling?.classList?.contains(
-      "accordion-text"
-    ) ||
-    presentAccordion?.nextElementSibling?.classList?.contains(
-      "accordion-text-active"
-    )
-  ) {
-    isAccordionOpen
-      ? openAccordion(presentAccordion?.nextElementSibling)
-      : closeAccordion(presentAccordion?.nextElementSibling);
-  } else {
-    console.log(`not found`);
-  }
-  previousAccordion = presentAccordion;
-  if (previousAccordion !== presentAccordion) {
-    console.log(`Prev : ${presentAccordion} , Curr :${presentAccordion}`);
-  }
-}
-
 function openAccordion(div) {
   div.classList.add("accordion-text-active");
   div.classList.remove("accordion-text");
-  isAccordionOpen = false;
 }
 function closeAccordion(div) {
   div.classList.remove("accordion-text-active");
   div.classList.add("accordion-text");
-  isAccordionOpen = true;
+}
+
+function multipleAccordionDiv(e) {
+  presentAccordion = e.target;
+  if (
+    presentAccordion?.lastElementChild?.classList?.contains("accordion-text")
+  ) {
+    openAccordion(presentAccordion?.lastElementChild);
+  } else if (
+    presentAccordion?.lastElementChild?.classList?.contains(
+      "accordion-text-active"
+    )
+  ) {
+    closeAccordion(presentAccordion?.lastElementChild);
+  } else if (
+    presentAccordion?.nextElementSibling?.nextElementSibling?.classList?.contains(
+      "accordion-text"
+    )
+  ) {
+    openAccordion(presentAccordion?.nextElementSibling?.nextElementSibling);
+  } else if (
+    presentAccordion?.nextElementSibling?.nextElementSibling?.classList?.contains(
+      "accordion-text-active"
+    )
+  ) {
+    closeAccordion(presentAccordion?.nextElementSibling?.nextElementSibling);
+  } else if (
+    presentAccordion?.nextElementSibling?.classList?.contains("accordion-text")
+  ) {
+    openAccordion(presentAccordion?.nextElementSibling);
+  } else if (
+    presentAccordion?.nextElementSibling?.classList?.contains(
+      "accordion-text-active"
+    )
+  ) {
+    closeAccordion(presentAccordion?.nextElementSibling);
+  } else {
+    console.log(`element not found`);
+  }
+}
+
+function simpleAccordion(e) {
+  presentAccordion = e.target;
+  if (
+    presentAccordion?.lastElementChild?.classList?.contains("accordion-text")
+  ) {
+    Array.from(accordionText).map((item) => {
+      if (item === presentAccordion?.lastElementChild) {
+        item?.classList?.remove("accordion-text");
+        item?.classList?.add("accordion-text-active");
+      } else {
+        item?.classList?.remove("accordion-text-active");
+        item?.classList?.add("accordion-text");
+      }
+    });
+  } else if (
+    presentAccordion?.nextElementSibling?.nextElementSibling?.classList?.contains(
+      "accordion-text"
+    )
+  ) {
+    Array.from(accordionText).map((item) => {
+      if (item === presentAccordion?.nextElementSibling?.nextElementSibling) {
+        item?.classList?.remove("accordion-text");
+        item?.classList?.add("accordion-text-active");
+      } else {
+        item?.classList?.remove("accordion-text-active");
+        item?.classList?.add("accordion-text");
+      }
+    });
+  } else if (
+    presentAccordion?.nextElementSibling?.classList?.contains("accordion-text")
+  ) {
+    Array.from(accordionText).map((item) => {
+      if (item === presentAccordion?.nextElementSibling) {
+        item?.classList?.remove("accordion-text");
+        item?.classList?.add("accordion-text-active");
+      } else {
+        item?.classList?.remove("accordion-text-active");
+        item?.classList?.add("accordion-text");
+      }
+    });
+  }
 }
 
 function check(e) {
   if (checkMultipleOpenAllowed()) {
-    openAccordion();
+    multipleAccordionDiv(e);
+    // close all open accordions.
   } else {
-    toggleAccordionDiv(e);
+    simpleAccordion(e);
   }
 }
 
