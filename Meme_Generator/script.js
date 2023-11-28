4ae12fb5b59137d5ab9457263e212978c15ceb05
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-11-28 21:32:05 +0530

    Adding skeleton laoder while fetch

--- Meme_Generator/script.js
+++ Meme_Generator/script.js
@@ -1,22 +1,35 @@
 let memeImg = document.querySelector(".meme-img");
-let memeTitle = document.querySelector(".title-info");
-let memeAuthor = document.querySelector(".author-info");
+let memeTitleInfo = document.querySelector(".title-info");
+let memeAuthorInfo = document.querySelector(".author-info");
 let generateMemeBtn = document.querySelector(".generate-meme-btn");
 let src = `https://meme-api.com/gimme/wholesomememes`;
-
+let loader = document.querySelectorAll(".loading");
+let loaderText = document.querySelectorAll(".loading-text");
 function getMemeResponse() {
+  //    skeltonLoader();
   fetch(src)
     .then((res) => res.json())
     .then((res) => {
-      memeTitle.innerText = res.title;
+      removeSkeletonLoader();
+      memeTitleInfo.innerText = res.title;
       memeImg.setAttribute("src", `${res.url}`);
-      memeAuthor.innerText = res.author;
+      memeAuthorInfo.innerText = res.author;
     })
     .catch((err) => console.warn(err));
 }
 
+function addSkeletonLoader() {
+  loader.forEach((item) => (item.style.display = "block"));
+  loaderText.forEach((item) => (item.style.display = "block"));
+}
+function removeSkeletonLoader() {
+  loader.forEach((item) => (item.style.display = "none"));
+  loaderText.forEach((item) => (item.style.display = "none"));
+}
+
 function generateMeme(e) {
-    e.preventDefault();
+  e.preventDefault();
+  addSkeletonLoader();
   getMemeResponse();
 }
 
