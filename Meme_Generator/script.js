0324ee08bbb9abb93631199a24158c79caff4fb0
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-11-29 11:53:57 +0530

    Code refactoring

--- Meme_Generator/script.js
+++ Meme_Generator/script.js
@@ -2,32 +2,32 @@ let memeImg = document.querySelector(".meme-img");
 let memeTitleInfo = document.querySelector(".title-info");
 let memeAuthorInfo = document.querySelector(".author-info");
 let generateMemeBtn = document.querySelector(".generate-meme-btn");
-let src = `https://meme-api.com/gimme/wholesomememes`;
 let loaderText = document.querySelectorAll(".loading-text");
 let loaderImg = document.querySelector(".img-loading");
+let src = `https://meme-api.com/gimme/wholesomememes`;
 
+function displayMeme(title, url, author) {
+  memeTitleInfo.innerText = title;
+  memeImg.setAttribute("src", url);
+  memeAuthorInfo.innerText = author;
+}
 function getMemeResponse() {
-  //    skeltonLoader();
   fetch(src)
     .then((res) => res.json())
     .then((res) => {
       removeSkeletonLoader();
-      memeTitleInfo.innerText = res.title;
-      memeImg.setAttribute("src", `${res.url}`);
-      memeAuthorInfo.innerText = res.author;
+      displayMeme(res.title, res.url, res.author);
     })
     .catch((err) => console.warn(err));
 }
 
 function addSkeletonLoader() {
-  // loader.forEach((item) => (item.style.display = "block"));
   loaderText.forEach((item) => (item.style.display = "block"));
   loaderImg.style.display = "block";
   memeAuthorInfo.style.opacity = "0%";
   memeTitleInfo.style.opacity = "0%";
 }
 function removeSkeletonLoader() {
-  // loader.forEach((item) => (item.style.display = "none"));
   loaderText.forEach((item) => (item.style.display = "none"));
   loaderImg.style.display = "none";
   memeAuthorInfo.style.opacity = "100%";
@@ -41,3 +41,4 @@ function generateMeme(e) {
 }
 
 generateMemeBtn.addEventListener("click", generateMeme);
+getMemeResponse();
